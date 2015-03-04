e61bb23a256a98126c0bf2809b0dfcaaabcddcf6
rotoshine@gmail.com <rotoshine@gmail.com>
2015-03-04 16:28:59 +0900

    .travis.yml 및 테스트 관련 추가

--- test/passport-kakao.spec.js
+++ test/passport-kakao.spec.js
@@ -1,3 +1,8 @@
-/**
- * Created by winterwolf on 2015-03-04.
- */
+'use strict';
+
+describe('passport-kakao', function() {
+    it('passport-kakao 객체가 제대로 생성이 되어 있어야 한다.', function() {
+        var Strategy = require('../lib/passport-kakao');
+        expect(Strategy).not.toBeNull();
+    });
+});
