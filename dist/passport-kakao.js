080116185a05608d7e1e9279ac9359ff3ac2a78c
rotoshine <rotoshine@gmail.com>
2019-10-04 19:10:35 +0900

    tsconfig.json 다시 생성

---
+++ dist/passport-kakao.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var strategy_1 = __importDefault(require("./strategy"));
+exports.Strategy = strategy_1.default;
+exports.default = strategy_1.default;
